64dced5b299df6864e86d272822c781d1934d849
bighoho123 <jinjinwudi@gmail.com>
2014-07-23 23:38:26 +0930

    Finish the landing page

--- index.php
+++ index.php
@@ -7,11 +7,10 @@
     <title>Absolute Juice</title>
 
     <!-- Bootstrap -->
-    <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
 
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <!-- Customize style sheet -->
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
 
 
 
@@ -23,11 +22,81 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class='banner'>
+      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+          <div class=""></div>
 
+          <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+          
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav" style='width:100%'>
+              <li class='col-sm-3'><a href="#" >排毒瘦身套装</a></li>
+              <li class='col-sm-3'><a href="#" >单品果汁</a></li>
+              <li class='col-sm-3'><a href="#" >关于A-Juice</a></li>
+              <li class='col-sm-3'><a href="#" >预定购买</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+              
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- Container of the content on the image -->
+      <div class='container'>
+        <div class='center-block logo-on-banner'>
+          <img src="assets/images/alliance.png" style='width:200px'>
+          <div class='clearfix' style='height:120px'></div>
+          <div style='font-size:60px'>ABSOLUTE</div>
+          <div class='row'>
+            <div class='hidden-xs col-sm-5 dash-in-middle'></div>
+            <div class='col-sm-2' style='font-size:40px;position:relative'>juice</div>
+            <div class='hidden-xs col-sm-5 dash-in-middle'></div>
+          </div>
+          
+          <div class='clearfix' style='height:10px'></div>
+          <div style='font-size:40px'>绝对的排毒果蔬汁专家</div>
+          <div style='font-size:30px'>排毒 瘦身 美肤</div>
+        </div>
+      
+      </div>
+    </div>
+
+    <div style="position:relative">
+      <div class='blur-border' style="height:20px;width:100%"></div>
+    </div>
+    
+
+    <div class='container-fluid' style='height:500px;background-color:rgb(98,135,77)'>
+      <div class='col-sm-8 col-sm-offset-2'>
+        <div class="jumbotron" style='background-image:url(assets/images/meditation.jpg);background-size:cover'>
+          <h1>什么是排毒</h1>
+          <p>排毒就是净化身体血液。主要通过肝脏去除血液中有害的杂质和毒素，例如药物，重金属，氨和一些化学物质，让身体保持健康活力</p>
+          <p>每个人的身体都有一个自然的排毒系统 但是不当的生活饮食习惯和外界恶劣的环境会造成的排毒系统紊乱或者超负荷，身体就无法进行正常排毒了</p>
+          <p><a class="btn btn-primary btn-lg" role="button">立即体验排毒</a></p>
+        </div>
+      <div>
+    </div>
+
+    <div class='container' style="height:600px">
+        <div class='row'>
+          <div class='col-sm-3'>1</div>
+          <div class='col-sm-3'>2</div>
+          <div class='col-sm-3'>3</div>
+          <div class='col-sm-3'>4</div>
+        </div>
+    </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  	<script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
